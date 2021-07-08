d6c3d50df28dd6554df356f5112414fa5e0a1ac4
listenyoon <84116086+listenyoon@users.noreply.github.com>
2021-07-08 23:28:11 +0900

    React JS Fundamentals Course #2.4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { directive } from '@babel/types';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+
+const foodILike = [{
+  id: 1,
+  name: "Quince", 
+  image: "https://t1.daumcdn.net/cfile/blog/2574844A53C1F2FD01",
+  rating: 5.0
+}, {
+  id: 2,
+  name: "Grape",
+  image: "https://snaped.fns.usda.gov/sites/default/files/styles/crop_ratio_7_5/public/seasonal-produce/2018-05/grapes_0.jpg?itok=uYYnmpTm",
+  rating: 4.9
+}, {
+  id: 3,
+  name: "Apple",
+  image: "https://www.irandriedfruit.com/wp-content/cache/thumbnails/2019/06/APPLE-1020x400-c.png",
+  rating: 4.8
+}];
+
+function Food({name, picture, rating}){
+  return <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture}></img>
+    </div>
+}
+
+//적절한 데이터가 있는지 확인, propTypes 이름은 강제.
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodILike.map(dish => (
+        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
+      ))}
     </div>
   );
 }
